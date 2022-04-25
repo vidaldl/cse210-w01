08641ecc01f80e62e7312d9ea6c74965136fda04
David Vidal <vidaldl@outlook.com>
2022-04-25 09:12:21 -0600

    Assignment 1 done

---
+++ Assignment 1-4/README.md
@@ -0,0 +1,2 @@
+# cse210-student-csharp-solo-prep-starter-60
+The starter code for .net 6.0
---
+++ Assignment 1-4/solo_prep_1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace solo_prep_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("This is solo prep 1");
+
+            // Write your code here
+            Console.Write("What is your first name? ");
+            string userName = Console.ReadLine();
+
+            Console.Write("What is your last name? ");
+            string userLast = Console.ReadLine();
+
+            Console.WriteLine($"Your name is {userLast}, {userName} {userLast}.");
+        }
+    }
+}
